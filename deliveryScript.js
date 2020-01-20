57d0a0f81f2093230e49c19d4d0d1b285d393c72
hicelus <ls.balboni@outlook.com>
2020-01-20 18:55:36 -0300

    0.8.1

--- deliveryScript.js
+++ deliveryScript.js
@@ -1,16 +1,30 @@
 var nome = document.getElementById("tNome");
 var endereco = document.getElementById("tEndereco");
 var mensagem = document.getElementById("instrucao");
+var campo = document.getElementById("caixa");
 
 //seleção de campo
 nome.onfocus = function(){instruir(1)};
 endereco.onfocus = function(){instruir(2)};
 
+//mouse fora do campo
+campo.onmouseout = function(){instruir(3)};
+
+
 function instruir(opcao){
+    switch(opcao){
+        case 1: mensagem.innerHTML = "PEDIDO: Insira seu nome:"; break;
+        case 2: mensagem.innerHTML = "PEDIDO: Insira seu Endereço:"; break;
+        case 3: mensagem.innerHTML = "Pedido"; break;
+    }
+
+
+    /*
     if(opcao == 1 ){
         mensagem.innerHTML = "PEDIDO: Insira seu nome:";
     }
     else if(opcao == 2){
         mensagem.innerHTML = "PEDIDO: Insira seu endereço:";
     }
+    */
 }
